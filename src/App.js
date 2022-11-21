8c40609c5fe797b16f7c8ccae6a9ecb0e43de534
villiamriegler <ville@riegler.se>
2022-11-21 19:38:00 +0100

    start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <p>Hello world</p>
+    <Nav/>
+  );
+}
+
+function Nav(){
+  return (
+    <header className='App-Header'>
+      <div class='Navbar'>
+        <div class='Logo'>
+          <img src='../public/logo512.png' className='logo' alt='Logo' />
+        </div>
+        <div class='Links'>
+          <ul class='Itemlist'>
+            <li class='Listitem'><a href='https://www.pornhub.com'>Friends</a></li>
+            <li class='Listitem'><a href='https://www.fuq.com'>Recomended</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 }
 
