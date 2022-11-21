d0d3550bc9a6c21396f6033cca8b01ceae6fe069
villiamriegler <ville@riegler.se>
2022-11-21 20:24:34 +0100

    Reset

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Nav/>
+    <p>Hello World</p>
   );
 }
 
-function Nav(){
-  return (
-    <header className='App-Header'>
-      <div class='Navbar'>
-        <div class='Logo'>
-          <img src='../public/logo512.png' className='logo' alt='Logo' />
-        </div>
-        <div class='Links'>
-          <ul class='Itemlist'>
-            <li class='Listitem'><a href='https://www.pornhub.com'>Friends</a></li>
-            <li class='Listitem'><a href='https://www.fuq.com'>Recomended</a></li>
-          </ul>
-        </div>
-      </div>
-    </header>
-  );
-}
+
 
 export default App;
