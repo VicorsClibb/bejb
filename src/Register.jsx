79ed4b43a0e5ecf54d28a50a61a1c4bfc39d905a
Victor Hansson Lindblom <v.hanssonlindblom2003@gmail.com>
2022-11-22 22:42:29 +0100

    m

--- src/Register.jsx
+++ src/Register.jsx
@@ -12,6 +12,9 @@ import React, {useState} from "react";
 return (
     <>
       <form onSubmit={handleSubmit}>
+      <label>Full name</label>
+      <input value={name} name="name" id="name" type="text" placeholder="Enter full name"/>
+
         <label htmlFor="email">Email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email or Username" id="email" name="email" />
 
@@ -19,7 +22,7 @@ return (
         <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
         <button>Login</button>
       </form>
-      <button>Don't have an account? Register</button>
+      <button>Already have an account? Login</button>
     </>
 )
 }
